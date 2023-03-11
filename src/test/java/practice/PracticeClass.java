c97f3c7f6c0ed410e4919373b3c78522ff3d5449
Aman <ak6838883@gmail.com>
2023-03-11 15:22:13 +0530

    first commit

---
+++ src/test/java/practice/PracticeClass.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("test1");
+	}
+
+}
---
+++ src/test/java/practice/PractticeClass2.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PractticeClass2 {
+	@Test
+	public void test()
+	{
+		System.out.println("test1------class2");
+	}
+}
